22f8ea58ad611d80e3271dbe491951aedd2580ea
Shray Jain <76147471+shray-jayn@users.noreply.github.com>
2024-11-09 09:51:15 +0530

    Added Prisma ORM

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as morgan from 'morgan';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const corsOrigins = process.env.CORS_ORIGIN
+    ? process.env.CORS_ORIGIN.split(',')
+    : ['http://localhost:4200'];
+
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (corsOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.use(morgan('dev'));
+
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+
 }
 bootstrap();
